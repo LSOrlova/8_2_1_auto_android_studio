b48dbe345be3d0496e0b6d6d348633246b6fb1a1
Luyba <lsorlova82@gmail.com>
2023-08-28 18:37:03 +0300

    fixes

--- app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -3,9 +3,12 @@ package com.netology.tabbedapplication;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 import android.view.View;
@@ -26,14 +29,14 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest1 {
+public class MainActivityTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest1() {
+    public void mainActivityTest() {
         ViewInteraction tabView = onView(
                 allOf(withContentDescription("Tab 2"),
                         childAtPosition(
@@ -44,6 +47,12 @@ public class MainActivityTest1 {
                         isDisplayed()));
         tabView.perform(click());
 
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.section_label), withText("Page: 2"),
+                        withParent(allOf(withId(R.id.constraintLayout),
+                                withParent(withId(R.id.view_pager)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Page: 2")));
 
         ViewInteraction tabView2 = onView(
                 allOf(withContentDescription("Tab 1"),
@@ -54,6 +63,13 @@ public class MainActivityTest1 {
                                 0),
                         isDisplayed()));
         tabView2.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.section_label), withText("Page: 1"),
+                        withParent(allOf(withId(R.id.constraintLayout),
+                                withParent(withId(R.id.view_pager)))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Page: 1")));
     }
 
     private static Matcher<View> childAtPosition(
